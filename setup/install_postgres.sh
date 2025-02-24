7e2ece6284436a939e07f535ef10694f150fb11a
James Beswick <jbeswick@stripe.com>
2025-02-24 17:38:35 +0000

    Dump GIT_VALID_PII_OVERRIDE

--- setup/install_postgres.sh
+++ setup/install_postgres.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 
 # Update Homebrew
-brew update
+##brew update
 
-# Install Postgres
-brew install postgresql
+# 1. Install Postgres
+##brew install postgresql
 
 # Start Postgres service
-brew services start postgresql
+##brew services start postgresql
 
 # Enable Postgres service to start on boot
-# Homebrew services are automatically enabled on boot
+#pg_ctl -D /usr/local/var/postgres start
 
 # Run Postgres service
-#pg_ctl -D /usr/local/var/postgres start
-brew services start postgresql@14
+##brew services start postgresql@14
 
 # Print Postgres version to verify installation
-psql --version
+##psql --version
+
+# Create blank database
+## createdb my-medusa-store
+
+# 3. Get the current username
+#CURRENT_USER=$(whoami)
+#psql -U $CURRENT_USER -d my-medusa-store
+
+# 2. Install Medusa
+#npm install -g @medusajs/medusa-cli
+#npx create-medusa-app@latest my-medusa-store
+#cd my-medusa-store
+#medusa new my-medusa-store
+
+# 3. Create admin user
+#npx medusa user -e admin@medusa-test.com -p supersecret
+
+git clone https://github.com/benjasl-stripe/workshop
+
+
+
+#pwd
+#4 Run dev server
+npm run dev
