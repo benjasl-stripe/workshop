e5482abaf8cddcf1676cc7f550f2d398406b6566
Ben Smith <benjasl@stripe.com>
2025-10-21 18:16:45 +0100

    Delete medusa-backend/src/modules/custom-payment-provider/index.ts

--- medusa-backend/src/modules/custom-payment-provider/index.ts
+++
@@ -1,7 +0,0 @@
-// src/modules/custom-payment-provider/index.ts
-import CustomPaymentProviderService from "./service";
-import { ModuleProvider, Modules } from "@medusajs/framework/utils";
-
-export default ModuleProvider(Modules.PAYMENT, {
-  services: [CustomPaymentProviderService],  // Register the service here
-});
